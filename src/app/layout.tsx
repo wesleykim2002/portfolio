fa1ba743092dbf38b65ecf9bff0af130da492ce9
Wesley Kim <wesleykim2002@gmail.com>
2026-02-05 23:50:31 -0500

    added footer
    Update Footer.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 
 const geistSans = Geist({
@@ -28,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    // TODO: Update footer content with links to social media and links to navigate to different sections of the website
+    <footer className="mt-16 text-center text-sm text-zinc-500">
+      &copy; {new Date().getFullYear()} Wesley Kim. All rights reserved.
+    </footer>
+  );
+}
